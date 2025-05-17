f02847b197a7130a774166bf0347160eee1ccaca
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-05-17 15:30:31 +0530

    Time: 0 ms (100%), Space: 11.6 MB (34.47%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        vector<int> res;
-        long long num = 0;
-        int i = 0;
-        while(i < digits.size()){
-            num = num*10 + digits[i];
-            i++;
+        for(int i = digits.size() - 1; i >= 0; i--){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
         }
-        num++;
-        while(num > 0){
-            res.push_back(num%10);
-            num = num/10;
-        }
-        reverse(res.begin() ,res.end());
-        return res;
+
+        digits.insert(digits.begin() , 1);
+        return digits;
     }
 };
