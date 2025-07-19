cc1102ea6178f159c81b0590a7df33a0c56dfe38
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-07-19 11:50:48 +0530

    Time: 0 ms (100%), Space: 18.2 MB (61.21%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int change = image[sr][sc];
-        if(color == change) return image;
+        int start = image[sr][sc];
+        if(color == start) return image;
+        queue<pair<int , int>> que;
         int rows = image.size();
         int cols = image[0].size();
-        queue<pair<int,int>> que;
         que.push({sr , sc});
         image[sr][sc] = color;
-
+        
         while(!que.empty()){
             int r = que.front().first;
             int c = que.front().second;
-            que.pop();
             
-            vector<pair<int , int>> dir = {{1,0} , {-1,0} , {0,1} , {0,-1}};
-
-            for(int i = 0 ; i < dir.size(); i++){
-                int nr = r + dir[i].first;
-                int nc = c + dir[i].second;
-
-                if(nr >= 0 && nr < rows && nc >= 0 && nc < cols && image[nr][nc] == change){
+            que.pop();
+            vector<pair<int , int>> s = {{1,0},{-1,0},{0,1},{0,-1}};
+            for(int i = 0 ; i < s.size(); i++){
+                int nr = r + s[i].first;
+                int nc = c + s[i].second;
+                if(nr >= 0 && nr < rows && nc >= 0 && nc < cols && image[nr][nc] == start){
                     image[nr][nc] = color;
-                    que.push({nr , nc});
+                    que.push({nr,nc});
+                    
                 }
             }
         }
