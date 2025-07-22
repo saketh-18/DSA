225e2964d9a8027e601a68f14d0db1c1b71bfc3f
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-07-22 19:37:51 +0530

    Time: 0 ms (100%), Space: 16.8 MB (75.27%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,53 +1,43 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        queue<pair<pair<int , int> , int>> que;
-
-        int rows = grid.size();
-        int cols = grid[0].size();
-
-        //storing positions of rotten oranges initially
-        for(int i = 0 ; i < grid.size(); i++){
-            for(int j = 0 ; j < grid[i].size(); j++){
-                if(grid[i][j] == 2){
-                    que.push({{i , j} , 0});
-                }
+        queue<pair<int, int>> que;
+        int row = grid.size(), col = grid[0].size();
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[i].size(); j++) {
+                if (grid[i][j] == 2)
+                    que.push({i, j});
             }
         }
 
-        // if(que.empty()) return -1;
-        int time = 0;
-        vector<pair<int,int>> directions = {{-1,0}, {1,0}, {0,-1}, {0,1}};
-        while(!que.empty()){
-            int r = que.front().first.first;
-            int c = que.front().first.second;
-            int t = que.front().second;
+        int minutes = 0;
+        while (!que.empty()) { 
+            vector<pair<int, int>> vec = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+            int size = que.size();
+            for(int s = 0 ; s < size ; s++) {
+                int rows = que.front().first;
+                int cols = que.front().second;
+                que.pop();
+                for (auto i : vec) {
+                    int nr = rows + i.first;
+                    int nc = cols + i.second;
 
-            que.pop();
-
-            time = max(time , t);
-
-            // checking the adjacent positions of the rotten orange
-
-            for(auto dir : directions) {
-                int nr = r + dir.first;
-                int nc = c + dir.second;
-
-                if(nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] == 1) {
-                    grid[nr][nc] = 2;
-                    que.push({{nr, nc}, t + 1});
+                    if (nr >= 0 && nr < row && nc >= 0 && nc < col) {
+                        if (grid[nr][nc] == 1) {
+                            grid[nr][nc] = 2;
+                            que.push({nr, nc});
+                        }
+                    }
                 }
             }
+            if(!que.empty()) minutes++;
         }
-
-        for(int i = 0 ; i < grid.size(); i++){
-            for(int j = 0 ; j < grid[i].size(); j++){
-                if(grid[i][j] == 1){
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[i].size(); j++) {
+                if (grid[i][j] == 1)
                     return -1;
-                }
             }
         }
-        return time;
-
+        return minutes;
     }
 };
