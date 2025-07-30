635a9359dfa10bd9cfcfa00d78fb493687e3ca65
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-07-30 19:53:34 +0530

    Time: 11 ms (11.56%), Space: 18.8 MB (35.84%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,41 +1,47 @@
 class Solution {
 public:
-    vector<int> findOrder(int nums, vector<vector<int>>& pre) {
-        //create an adjacency list 
-        //use kahn's algo
-        //return topo array if topo.size() == nums
-        //else return empty array
-
-        vector<vector<int>> adj(nums);
-        vector<int> id(nums);
-        for(int i = 0 ; i < pre.size(); i++){
-            adj[pre[i][1]].push_back(pre[i][0]);
-            id[pre[i][0]]++;
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        // Step 1: Build the adjacency list 
+        vector<vector<int>> adj(numCourses);
+        for (auto it : prerequisites) {
+            adj[it[1]].push_back(it[0]);
         }
 
-        queue<int> que;
+        // Step 2: Calculate in-degrees 
+        vector<int> indegree(numCourses, 0);
+        for (int i = 0; i < numCourses; i++) {
+            for (auto it : adj[i])
+                indegree[it]++;
+        }
+
+        // Step 3: Push all courses with 0 prerequisites into the queue
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        // Step 4: Perform BFS (Topological Sort) to determine a valid order
         vector<int> topo;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);  // This course can now be taken
 
-        for(int i = 0; i < nums;i++){
-            if(id[i] == 0){
-                que.push(i);
+            // Reduce the in-degree of all dependent courses
+            for (auto it : adj[node]) {
+                indegree[it]--;
+                if (indegree[it] == 0) {
+                    q.push(it);  // This course is now ready to be taken
+                }
             }
         }
 
-        if(que.empty()) return {};
+        // Step 5: If we managed to take all courses, return the order
+        if (topo.size() == numCourses) return topo;
 
-        while(!que.empty()){
-            int node = que.front();
-            que.pop();
-            topo.push_back(node);
-
-            for(int i : adj[node]){
-                id[i]--;
-                if(id[i] == 0) que.push(i);
-            }
-        }
-
-        if(topo.size() == nums) return topo;
-        else return {};
+        // Otherwise, there's a cycle (some courses can't be completed)
+        return {};
     }
 };
