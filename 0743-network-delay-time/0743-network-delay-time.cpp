7a18741c65a23024819bb6ac13b9e32b33e0f3a9
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-08-03 16:57:23 +0530

    Time: 74 ms (90.28%), Space: 44.5 MB (55.91%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        //create an adjacency list
+        //create a priority que for djikstra
+        //do bfs
+
+        vector<vector<pair<int,int>>> adj(n+1);
+        for(int i = 0 ; i < times.size(); i++){
+            adj[times[i][0]].push_back({times[i][1] , times[i][2]});
+        }
+
+        vector<int> dist(n+1, INT_MAX);
+        priority_queue<pair<int,int> , vector<pair<int,int>> , greater<pair<int,int>>> pq;
+
+        pq.push({0,k});
+        while(!pq.empty()){
+            auto [cost,node] = pq.top();
+            pq.pop();
+            if(dist[node] < cost) continue;
+            dist[node] = cost;
+            dist[0] = -1;
+
+            for(auto i : adj[node]){
+                if(dist[i.first] > cost + i.second){
+                    dist[i.first] = cost+i.second;
+                    pq.push({cost+i.second, i.first});
+                }
+            }
+        }
+        int ans = 0;
+        for(int i = 0 ; i < dist.size(); i++){
+            if(dist[i] == INT_MAX) return -1;
+            else ans = max(dist[i],ans);
+        }
+        return ans;
+    }
+};
