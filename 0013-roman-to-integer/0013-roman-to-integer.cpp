d3378332f67821fcc21fa42128884d9d9f720ff8
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-08-09 21:33:46 +0530

    Time: 4 ms (67.11%), Space: 13.3 MB (49.34%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,27 +1,26 @@
+#include <unordered_map>
+#include <string>
+using namespace std;
+
 class Solution {
 public:
     int romanToInt(string s) {
-        //traverse from left to right
-        //if ab , a > b normal , if b > a then b - a
-
-        unordered_map<char,int> mpp;
-        mpp['I'] = 1;
-        mpp['V'] = 5;
-        mpp['X'] = 10;
-        mpp['L'] = 50;
-        mpp['C'] = 100;
-        mpp['D'] = 500;
-        mpp['M'] = 1000;
-
-        int ans = 0;
-        for(int i = 0 ; i < s.length(); i++){
-            if(i < s.length() - 1 && mpp[s[i]] < mpp[s[i+1]]){
-                ans += (mpp[s[i+1]] - mpp[s[i]]);
-                cout << mpp[s[i+1]] - mpp[s[i]] << endl;
-                i++;
+        unordered_map<char, int> roman = {
+            {'I',1}, {'V',5}, {'X',10}, {'L',50},
+            {'C',100}, {'D',500}, {'M',1000}
+        };
+        
+        int result = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            int curr = roman[s[i]];
+            int next = (i+1 < s.size()) ? roman[s[i+1]] : 0;
+            
+            if (curr < next) {
+                result -= curr;
+            } else {
+                result += curr;
             }
-            else ans += mpp[s[i]]; cout << mpp[s[i]] << endl;
         }
-        return ans;
+        return result;
     }
 };
