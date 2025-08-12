4b92503e3fe3dd5ef6c4986ed2687742ec347790
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-08-12 11:46:32 +0530

    Time: 3 ms (6.99%), Space: 9.1 MB (88.19%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    void dfs(string &digits, int i , string cur, vector<string> &ans, vector<string> &nums){
+    void dfs(string &digits, int i , string &cur, vector<string> &ans, vector<string> &nums){
         if(cur.length() == digits.length()){
             ans.push_back(cur);
             return ;
         }
 
-        if(i > digits.length()) return;
+        // if(i > digits.length()) return;
 
         for(char al : nums[digits[i]-'0']){
             cur.push_back(al);
