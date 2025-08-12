332d275c90a6fe544211c8afc075fd2c1954e876
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-08-12 15:50:20 +0530

    Time: 352 ms (51.28%), Space: 11.7 MB (35.03%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -3,21 +3,27 @@ public:
     
     vector<pair<int,int>> dir = {{0,1},{0,-1},{1,0},{-1,0}};
     bool dfs(int i , int j , vector<vector<char>> &board, string word , int k){
-        if(k == word.size()) return true;
+        if (board[i][j] != word[k]) return false;       // check current cell
+        if (k == word.size() - 1) return true; 
+
+        char temp = board[i][j];
+        board[i][j] = '#';
+
 
         for(auto n : dir){
             int nr = i + n.first;
             int nc = j + n.second;
 
             if(nr >= 0 && nc >= 0 && nr < board.size() && nc < board[0].size()){
-                if(board[nr][nc] == word[k]){
-                    char temp = board[nr][nc];
-                    board[nr][nc] = '#';
-                   if(dfs(nr,nc,board,word,k+1)) return true;
-                   board[nr][nc] = temp;
+                if(board[nr][nc] == word[k+1]){
+                    if(dfs(nr,nc,board,word,k+1)) {
+                        board[i][j] = temp;
+                        return true; 
+                    }
                 }
             }
         }
+        board[i][j] = temp;
         return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
