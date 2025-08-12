75c8880c1c1e087951469d53a835ceb5624acf6d
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-08-12 11:38:43 +0530

    Time: 0 ms (100%), Space: 9.1 MB (88.19%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void dfs(string &digits, int i , string cur, vector<string> &ans, vector<string> &nums){
+        if(cur.length() == digits.length()){
+            ans.push_back(cur);
+            return ;
+        }
+
+        if(i > digits.length()) return;
+
+        for(char al : nums[digits[i]-'0']){
+            cur.push_back(al);
+            dfs(digits,i+1,cur,ans,nums);
+            cout << cur << endl;
+            cur.pop_back();
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        if(digits.size() == 0) return {};
+        vector<string> nums = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        string cur;
+        vector<string> ans;
+        dfs(digits,0,cur,ans,nums);
+        return ans;
+    }
+};
