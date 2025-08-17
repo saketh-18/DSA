5005cdcf8d3c7e3692b86db5a19657bd50cf8235
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-08-17 09:26:59 +0530

    Time: 0 ms (100%), Space: 10 MB (97.33%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -9,17 +9,29 @@ public:
         return dp[i] = max(nums[i]+dfs(i-2,nums,dp),dfs(i-1,nums,dp));
     }
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size(),-1);
+        // vector<int> dp(nums.size(),-1);
         // return dfs(nums.size()-1,nums,dp);
         //tabulation => 
-        dp[0] = nums[0];
-        for(int i = 1; i < nums.size(); i++){
-            if(i < 2){
-                dp[i] = max(nums[i],nums[i-1]);
-            } else {
-                dp[i] = max(dp[i-1] , nums[i]+dp[i-2]);
-            }
+        // dp[0] = nums[0];
+        // for(int i = 1; i < nums.size(); i++){
+        //     if(i < 2){
+        //         dp[i] = max(nums[i],nums[i-1]);
+        //     } else {
+        //         dp[i] = max(dp[i-1] , nums[i]+dp[i-2]);
+        //     }
+        // }
+        //space optimization approach =>
+
+        if(nums.size() == 1) return nums[0];
+        if(nums.size() == 2) return max(nums[1],nums[0]);
+        int prev = max(nums[1],nums[0]), prev2 = nums[0];
+
+        for(int i = 2; i < nums.size(); i++){
+            int cur = max(nums[i] + prev2 , prev);
+            prev2 = prev;
+            prev = cur;
         }
-        return dp[nums.size() - 1];
+        return prev;
+        // return dp[nums.size() - 1];
     }
 };
