cce672f198019497aed029c24861144c625eb4ac
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-08-17 09:20:52 +0530

    Time: 0 ms (100%), Space: 10.7 MB (22.65%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -10,6 +10,16 @@ public:
     }
     int rob(vector<int>& nums) {
         vector<int> dp(nums.size(),-1);
-        return dfs(nums.size()-1,nums,dp);
+        // return dfs(nums.size()-1,nums,dp);
+        //tabulation => 
+        dp[0] = nums[0];
+        for(int i = 1; i < nums.size(); i++){
+            if(i < 2){
+                dp[i] = max(nums[i],nums[i-1]);
+            } else {
+                dp[i] = max(dp[i-1] , nums[i]+dp[i-2]);
+            }
+        }
+        return dp[nums.size() - 1];
     }
 };
