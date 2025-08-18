622f01bfd67578e9c898492922630fb1bf44a701
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-08-18 13:46:47 +0530

    Time: 1 ms (45.08%), Space: 16.3 MB (62.17%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -18,6 +18,19 @@ public:
     }
     int minPathSum(vector<vector<int>>& grid) {
       vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size(),-1));
-      return dfs(grid.size()-1,grid[0].size()-1,grid,dp);
+      // return dfs(grid.size()-1,grid[0].size()-1,grid,dp);
+      //tabulation approach
+      //base case =>
+      dp[0][0] = grid[0][0];
+      for(int i = 0 ; i < grid.size(); i++){
+        for(int j = 0 ; j < grid[0].size(); j++){
+          if(j == 0 && i == 0) continue;
+          int right = INT_MAX, down = INT_MAX;
+          if(i > 0) right = grid[i][j] + dp[i-1][j];
+          if(j > 0) down = grid[i][j] + dp[i][j-1];
+          dp[i][j] = min(right,down);
+        }
+      }
+      return dp[grid.size()-1][grid[0].size()-1];
     }
 };
