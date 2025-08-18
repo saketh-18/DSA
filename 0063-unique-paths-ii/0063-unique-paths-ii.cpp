a6985a82be8d19897199b65bb3d7618d616c5319
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-08-18 13:02:52 +0530

    Time: 3 ms (11.44%), Space: 11.9 MB (54.74%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int dfs(int i , int j, vector<vector<int>> &grid, vector<vector<int>> &dp){
+      if(i == 0 && j == 0) return dp[0][0] = 1;
+      if(i < 0 || j < 0) return 0;
+
+      //should go top and left;
+      //only call dfs if there is no obstacle
+
+      int top = 0 , left = 0;
+      if(dp[i][j] != -1) return dp[i][j];
+      if(i > 0 && grid[i-1][j] != 1){
+        top = dfs(i-1,j,grid,dp);
+      }
+      if(j > 0 && grid[i][j-1] != 1){
+        left = dfs(i,j-1,grid,dp);
+      }
+      return dp[i][j] = top + left;
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        //use recursion();
+        int m = obstacleGrid.size()-1;
+        int n = obstacleGrid[0].size()-1;
+        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        if(obstacleGrid[m][n] == 1) return 0;
+        return dfs(m,n,obstacleGrid,dp);
+    }
+};
