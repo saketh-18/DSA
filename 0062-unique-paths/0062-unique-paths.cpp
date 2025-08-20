7d782fbfaa29ce45f36d867c217087be9b489cb0
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-08-20 22:00:15 +0530

    Time: 0 ms (100%), Space: 9.3 MB (34.02%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -7,8 +7,16 @@ public:
         return dp[i][j] = dfs(i-1,j,dp) + dfs(i,j-1,dp);
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return dfs(m-1,n-1,dp);
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        dp[0][0] = 1;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 && j == 0) continue;
+                if(i > 0) dp[i][j] += dp[i-1][j];
+                if(j > 0) dp[i][j] += dp[i][j-1]; 
+            }
+        }
         return dp[m-1][n-1];
+        // return dfs(m-1,n-1,dp);
     }
 };
