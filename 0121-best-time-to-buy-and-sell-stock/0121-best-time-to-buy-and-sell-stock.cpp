80f54ac66f224f907057b0886dcaa6e6e82f84f8
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-08-22 11:51:52 +0530

    Time: 92 ms (6.63%), Space: 96.1 MB (99.98%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-            int l = prices[0] , len = 0;
+        if(prices.size() == 1){
+            return 0;
+        }
+        else {
+            int l = prices[0] , len = 0 , sum = 0;
             for(int i = 1; i < prices.size(); i++){
-                if(prices[i] > l){
-                    len = max(len , prices[i] - l); 
+                if(prices[i] - l > 0){
+                    sum = prices[i] - l;
+                    len = max(len , sum);
                 }
                 else {
                     l = prices[i];
                 }
             }
             return len;
+        }
+        return 0;
     }
 };
