c79cb03957a258a9cc0dda0eee1a139d7f33dc5c
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-08-31 11:12:14 +0530

    Time: 15 ms (53.05%), Space: 25.8 MB (25.64%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
-    int dfs(int i, int sum, int &target, vector<int> &nums){
+    int dfs(int i, int sum, int &target, vector<int> &nums,  vector<vector<int>> &dp, int &ind){
         if(i >= nums.size()){
             if(target == sum) return 1;
             return 0;
         }
-
-        int plus = dfs(i+1,sum+nums[i],target,nums);
-        int minus = dfs(i+1,sum-nums[i],target,nums);
-        return plus + minus;
+        
+        if(dp[i][sum + ind] != -1) return dp[i][sum+ind];
+        int plus = dfs(i+1,sum+nums[i],target,nums,dp,ind);
+        int minus = dfs(i+1,sum-nums[i],target,nums,dp,ind);
+        
+        return dp[i][sum + ind] = plus + minus;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        return dfs(0,0,target, nums);
+        int sum = 0;
+        for(int i : nums){
+            sum += i;
+        }
+        vector<vector<int>> dp(nums.size(), vector<int>(2*sum + 1,-1));
+        return dfs(0,0,target,nums,dp,sum);
     }
 };
