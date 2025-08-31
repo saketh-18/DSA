e48745ad2aa6e267818f7d6894b28fa074674eda
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-08-31 12:27:08 +0530

    Time: 18 ms (59.8%), Space: 46.1 MB (17.87%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -12,7 +12,26 @@ public:
         return dp[i][amt] = dfs(i-1,amt,coins,dp) + dfs(i,amt-coins[i], coins,dp);
     }
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>> dp(coins.size(), vector<int>(amount + 1,-1));
-        return dfs(coins.size()-1,amount,coins,dp);
+        vector<vector<long long>> dp(coins.size(), vector<long long>(amount + 1,0));
+        //tabulation
+        //base case
+        int n =  coins.size();
+        for(int i = 1; i < amount+1;i++){
+            if(i%coins[0] == 0) dp[0][i] = 1;
+        }
+        for(int i = 0; i < n;i++){
+            dp[i][0] = 1;
+        }
+
+        for(int i = 1; i < n;i++){
+            for(int j = 1; j < amount+1; j++){
+                long long not_take = dp[i-1][j];
+                long long take = 0;
+                if(j >= coins[i]) take = dp[i][j-coins[i]];
+                dp[i][j] = take + not_take;
+                if(dp[i][j] > INT_MAX) return 0;
+            }
+        }
+        return (int)dp[n-1][amount];
     }
 };
