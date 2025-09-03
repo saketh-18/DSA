8ee6632f8ce58d531efcbf155b3f7b8bdd571456
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-09-03 17:53:07 +0530

    Time: 0 ms (100%), Space: 12.6 MB (95.97%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,23 +12,12 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        queue<TreeNode*> q1, q2;
-        q1.push(p);
-        q2.push(q);
-        
-        while (!q1.empty() && !q2.empty()) {
-            TreeNode* n1 = q1.front(); q1.pop();
-            TreeNode* n2 = q2.front(); q2.pop();
-            
-            if (!n1 && !n2) continue;
-            if (!n1 || !n2 || n1->val != n2->val) return false;
-            
-            q1.push(n1->left);
-            q1.push(n1->right);
-            q2.push(n2->left);
-            q2.push(n2->right);
-        }
-        
-        return q1.empty() && q2.empty();
+        if(p == nullptr && q == nullptr) return true;
+        if(p == nullptr && q != nullptr) return false; 
+        if(p != nullptr && q == nullptr) return false;
+        if(p->val == q->val){
+            return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+        } 
+        return false;
     }
 };
