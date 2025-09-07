6e66e5f51ab462d67171e09ce56bfe46b88440af
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-09-07 11:44:06 +0530

    Time: 0 ms (100%), Space: 18.7 MB (51.33%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,29 +1,73 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<vector<int>> zeroes;
-        for(int i = 0 ; i < matrix.size(); i++){
-            for(int j = 0 ; j < matrix[i].size(); j++){
+        //set zeroes in first row and col
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        bool isrow = false, iscol = false;
+        bool isall = matrix[0][0] == 0;
+        for(int i = 0; i < rows; i++){
+            if(matrix[i][0] == 0){
+                isrow = true;
+                break;
+            }
+        }
+
+
+        for(int i = 0 ; i < cols; i++){
+            if(matrix[0][i] == 0){
+                iscol = true;
+                break;
+            }
+        }
+
+
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
                 if(matrix[i][j] == 0){
-                    zeroes.push_back({i , j});
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
         }
 
-        for (int i = 0; i < zeroes.size(); i++) {
-            int row = zeroes[i][0];
-            for (int j = 0; j < matrix[0].size(); j++) {
-                matrix[row][j] = 0;
+        //set zero rows
+        for(int i = 1; i < rows; i++){
+            if(matrix[i][0] == 0){
+                for(int j = 0; j < cols; j++){
+                    matrix[i][j] = 0; 
+                }
             }
         }
 
-        // Set entire columns to zero
-        for (int i = 0; i < zeroes.size(); i++) {
-            int col = zeroes[i][1];
-            for (int j = 0; j < matrix.size(); j++) {
-                matrix[j][col] = 0;
+        //set zero cols
+        for(int i = 1; i < cols; i++){
+            if(matrix[0][i] == 0){
+                for(int j = 0; j < rows; j++){
+                    matrix[j][i] = 0;
+                }
             }
         }
-        
+
+        if(isall){
+            for(int i = 0 ; i < rows;i++){
+                matrix[i][0] = 0;
+            }
+            for(int i = 0 ; i < cols;i++){
+                matrix[0][i] = 0;
+            }
+        }
+
+        if(isrow) {
+            for(int i = 0 ; i < rows;i++){
+                matrix[i][0] = 0;
+            }
+        }
+
+        if(iscol){
+            for(int i = 0 ; i < cols;i++){
+                matrix[0][i] = 0;
+            }
+        }
     }
 };
