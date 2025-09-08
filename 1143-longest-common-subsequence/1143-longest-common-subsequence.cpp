707cb02e5360c966e6ad76c4a1e2b4ee2a5e01ea
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-09-08 10:14:43 +0530

    Time: 38 ms (31.19%), Space: 27.7 MB (6.13%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int rec(int i, int j, string &a, string &b, vector<vector<int>> &dp){
-        if(i < 0 || j < 0) return 0;
+        if(i == 0 || j == 0) return 0;
 
         if(dp[i][j] != -1) return dp[i][j];
-        if(a[i] == b[j]){
+        if(a[i-1] == b[j-1]){
             return dp[i][j] =  1 + rec(i-1,j-1,a,b,dp);
         } 
 
         return dp[i][j] =  max(rec(i-1,j,a,b,dp),rec(i,j-1,a,b,dp));
     }
     int longestCommonSubsequence(string text1, string text2) {
-        vector<vector<int>> dp(text1.length(),vector<int>(text2.length(),-1));
-        return rec(text1.length()-1,text2.length()-1,text1,text2,dp);
+        vector<vector<int>> dp(text1.length()+1,vector<int>(text2.length()+1,-1));
+
+        return rec(text1.length(),text2.length(),text1,text2,dp);
     }
 };
