018ed9cfb73538b68d9b4fc5d2cfe20c6aabb167
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-09-10 18:13:00 +0530

    Time: 0 ms (100%), Space: 17.2 MB (69.13%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int rec(int i, int j, vector<int> &prices){
+        if(j == prices.size()){
+            i++;
+            j = i + 1;
+        }
+        if(i == prices.size()){
+            return 0;
+        }
+
+        int left = prices[j]-prices[i];
+        if(left > 0){
+            return left + rec(i+1,j+1,prices);
+        } else {
+            return rec(i+1,j+1,prices);
+        }
+    }
+    int maxProfit(vector<int>& prices) {
+        //for every day there are two possibilities 
+        //to buy at the current prices and to sell at the current price
+        //and store the profit  
+        return rec(0,1,prices);
+    }
+};
