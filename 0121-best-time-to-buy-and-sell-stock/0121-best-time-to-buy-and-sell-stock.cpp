29b70d0612ef384bda4473f5e9d85b9bc4bb22c4
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-09-10 18:02:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        if(prices.size() == 1){
-            return 0;
-        }
-        else {
-            int l = prices[0] , len = 0 , sum = 0;
-            for(int i = 1; i < prices.size(); i++){
-                if(prices[i] - l > 0){
-                    sum = prices[i] - l;
-                    len = max(len , sum);
-                }
-                else {
-                    l = prices[i];
-                }
+        //brute force
+        //computing difference for every pair of i and j (indices) and finding max among them   
+        int ans = 0;
+
+        for(int i = 0; i < prices.size(); i++){
+            for(int j = i + 1; j < prices.size(); j++){
+                ans = max(ans , prices[j]-prices[i]);
             }
-            return len;
         }
-        return 0;
+        return ans;
     }
 };
