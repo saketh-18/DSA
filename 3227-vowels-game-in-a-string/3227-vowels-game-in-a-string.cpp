a5032d4b0f695b3984ca24f66065fd37dbeef190
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-09-12 06:54:09 +0530

    Time: 24 ms (5.6%), Space: 19.3 MB (16.23%) - LeetHub

--- 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
@@ -1,23 +1,11 @@
 class Solution {
 public:
-    bool check(char c){
-        switch(c){
-            case 'a':
-            case 'e':
-            case 'i':
-            case 'o':
-            case 'u':
-                return true;
-        }
-        return false;
-    }
     bool doesAliceWin(string s) {
         int count = 0;
+        string vowels = "aeiou";
         for(char c : s){
-            if(check(c)){
-                count++;
-            }
+            if(vowels.find(c) != string::npos) return true;
         } 
-        return !(count == 0);
+        return false;
     }
 };
