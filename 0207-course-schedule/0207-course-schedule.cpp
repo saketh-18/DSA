ff98af0b30bd9d6d483d9872fbbe2a43ba220367
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-09-20 20:15:42 +0530

    Time: 4 ms (67.93%), Space: 19.4 MB (63.86%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
-    bool canFinish(int nums, vector<vector<int>>& pre) {
-        //create an adjacency list
-        //created indegree array
-        //create topo array
-
-        //adjacency list
-        vector<vector<int>> adj(nums);
-        vector<int> id(nums);
-        for(int i = 0 ; i < pre.size(); i++){
-            adj[pre[i][1]].push_back(pre[i][0]);
-            id[pre[i][0]]++;
-        }
-
-        queue<int> que;
-        for(int i = 0 ; i < nums;i++){
-            if(id[i] == 0){
-                que.push(i);
+    void topo(int i, vector<int> &vis, vector<vector<int>> &adj, int parent){
+        vis[i] = 1;
+        for(int course : adj[i]){
+            if(vis[course] == 1) return;
+            if(vis[course] == 0){
+                topo(course, vis, adj, i);
             }
         }
-
-        // if(que.size() == 0) return true;
-        // vector<int> topo;
-        int cnt = 0;
-        while(!que.empty()){
-            int node = que.front();
-            que.pop();
-            cnt++;
-
-            for(int i : adj[node]){
-                id[i]--;
-                // if(id[i] < 0) return true;
-                if(id[i] == 0) que.push(i);
-            }
-        }
-        return cnt == nums;
+        vis[i] = 2;
     }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+
+        //make an adjacency list out of the given prerequisites.
+        for(int i = 0; i < prerequisites.size(); i++){
+            adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        }
+
+        //just do topo sort;
+        vector<int> vis(numCourses);
+        for(int i = 0 ; i < numCourses; i++){
+            if(vis[i] == 0){
+                topo(i, vis, adj, -1);
+            }
+        }
+
+    //    return topo(0,vis,adj,-1);
+
+        int count = 0;
+        for(int i : vis){
+            i == 2 ? count++ : count = count;
+        }
+        return count == numCourses;
+    }
+
 };
