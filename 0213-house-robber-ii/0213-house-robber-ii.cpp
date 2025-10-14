91b9963d6230dee0ed2fd6c64127344f3b6c507d
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-10-14 06:51:31 +0530

    Time: 0 ms (100%), Space: 10.7 MB (55.91%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,28 +1,40 @@
 class Solution {
 public:
+    int dfs(int i, vector<int>& nums, vector<int>& dp) {
+        if (i < 0)
+            return 0;
+
+        if (i == 0)
+            return nums[i];
+
+        if (dp[i] != -1)
+            return dp[i];
+        int take = nums[i] + dfs(i - 2, nums, dp);
+        int not_take = dfs(i - 1, nums, dp);
+
+        return dp[i] = max(take, not_take);
+    }
+
+    int dfs1(int i, vector<int>& nums, vector<int>& dp) {
+        if (i < 1)
+            return 0;
+
+        if (i == 1)
+            return nums[i];
+
+        if (dp[i] != -1)
+            return dp[i];
+        int take = nums[i] + dfs1(i - 2, nums, dp);
+        int not_take = dfs1(i - 1, nums, dp);
+
+        return dp[i] = max(take, not_take);
+    }
     int rob(vector<int>& nums) {
         if(nums.size() == 1) return nums[0];
-        if(nums.size() == 2) return max(nums[1],nums[0]);
-        int prev = max(nums[1],nums[0]), prev2 = nums[0];
-
-        for(int i = 2; i < nums.size() - 1; i++){
-            int cur = max(nums[i] + prev2 , prev);
-            prev2 = prev;
-            prev = cur;
-        }
-
-        int first = prev;
-
-        prev = max(nums[2],nums[1]);
-        prev2 = nums[1];
-        for(int i = 3; i < nums.size(); i++){
-            int cur = max(nums[i] + prev2 , prev);
-            prev2 = prev;
-            prev = cur;
-        }
-        int second = prev;
-
-        return max(first,second);
-        // return dp[nums.size() - 1];
+        vector<int> dp(nums.size(), -1);
+        int ans1 = dfs(nums.size() - 2, nums,dp);
+        fill(dp.begin(), dp.end() , -1);
+        int ans2 = dfs1(nums.size()-1, nums,dp);
+        return max(ans1, ans2);
     }
 };
