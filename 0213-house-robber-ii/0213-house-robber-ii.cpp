948983b0fc37a6dbdb67c9ebd27ab39f552b572a
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-10-14 07:00:52 +0530

    Time: 0 ms (100%), Space: 10.7 MB (63.79%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,34 +1,5 @@
 class Solution {
 public:
-    int dfs(int i, vector<int>& nums, vector<int>& dp) {
-        if (i < 0)
-            return 0;
-
-        if (i == 0)
-            return nums[i];
-
-        if (dp[i] != -1)
-            return dp[i];
-        int take = nums[i] + dfs(i - 2, nums, dp);
-        int not_take = dfs(i - 1, nums, dp);
-
-        return dp[i] = max(take, not_take);
-    }
-
-    int dfs1(int i, vector<int>& nums, vector<int>& dp) {
-        if (i < 1)
-            return 0;
-
-        if (i == 1)
-            return nums[i];
-
-        if (dp[i] != -1)
-            return dp[i];
-        int take = nums[i] + dfs1(i - 2, nums, dp);
-        int not_take = dfs1(i - 1, nums, dp);
-
-        return dp[i] = max(take, not_take);
-    }
     int rob(vector<int>& nums) {
         if(nums.size() == 1) return nums[0];
         vector<int> dp(nums.size(), 0);
@@ -38,26 +9,32 @@ public:
         //without last element
         int take = 0;
         int not_take = 0;
+        int iminus1 = nums[0];
+        int iminus2 = 0;
         for(int i = 1; i < nums.size() - 1; i++){
             take = nums[i];
-            if(i > 1) take += dp[i-2];
-            not_take = dp[i-1];
-            dp[i] = max(not_take,take);
+            if(i > 1) take += iminus2;
+            not_take = iminus1;
+            int curr = max(not_take,take);
+            iminus2 = iminus1;
+            iminus1 = curr;
         }
 
-        int ans1 = dp[nums.size() - 2];
+        int ans1 = iminus1;
         //without first element
-        fill(dp.begin(),dp.end(),0);
-        dp[1] = nums[1];
+        iminus1 = nums[1];
+        iminus2 = 0;
         for(int i = 2; i < nums.size(); i++){
             take = nums[i];
-            if(i > 2) take += dp[i-2];
-            not_take = dp[i-1];
+            if(i > 2) take += iminus2;
+            not_take = iminus1;
 
-            dp[i] = max(take,not_take);
+            int curr = max(take,not_take);
+            iminus2 = iminus1;
+            iminus1 = curr;
         }
 
-        int ans2 = dp[nums.size() - 1];
+        int ans2 = iminus1;
 
         return max(ans2, ans1);
     }
