fe874c9509b39608abc8cab4325db9c5672338ee
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-10-18 10:25:41 +0530

    Time: 0 ms (100%), Space: 9.3 MB (36.92%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
-    int dfs(int i, int j,vector<vector<int>> &dp){
-        if(i == 0 && j == 0) return dp[0][0] = 1;
+    int dfs(int i, int j){
+        if(i == 0 && j == 0) return 1;
         if(i < 0 || j < 0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        return dp[i][j] = dfs(i-1,j,dp) + dfs(i,j-1,dp);
+
+        return dfs(i-1,j) + dfs(i,j-1);
     }
-    int uniquePaths(int m, int n) {
+    int uniquePaths(int m, int n) {   
+        //tabulation
+
         vector<vector<int>> dp(m,vector<int>(n,0));
         dp[0][0] = 1;
+
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(i == 0 && j == 0) continue;
-                if(i > 0) dp[i][j] += dp[i-1][j];
-                if(j > 0) dp[i][j] += dp[i][j-1]; 
+                int top = 0;
+                if(i >= 1) top = dp[i-1][j];
+                int left = 0;
+                if(j >= 1) left = dp[i][j-1];
+
+                dp[i][j] = left + top;
             }
-        }
-        return dp[m-1][n-1];
-        // return dfs(m-1,n-1,dp);
+        } 
+        return dp[m-1][n-1];    
     }
 };
