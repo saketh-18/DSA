4a41df7a7f3b5facfedf5ea124bb1f6d4f784eca
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-10-20 06:35:42 +0530

    Time: 0 ms (100%), Space: 17.7 MB (42.92%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x = 0;
+        for(string s : operations){
+            if(s[0] == '+' || s[2] == '+') x++;
+            else if(s[0] == '-' || s[2] == '-') x--;
+        }
+        return x;
+    }
+};
