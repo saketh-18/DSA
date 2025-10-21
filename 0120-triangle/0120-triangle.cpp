67f687d086522fe3c4f442d5f1df0cf701ea0a37
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-10-21 20:01:34 +0530

    Time: 0 ms (100%), Space: 13.4 MB (15.59%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,26 +1,44 @@
 class Solution {
 public:
-    int minimumTotal(vector<vector<int>>& triangle) {
+    int dfs(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &dp){
+        if(i >= grid.size()){
+            return 0;
+        }
+
+        if(j >= grid[i].size()) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+        int down = grid[i][j] + dfs(i+1,j,grid,dp);
+        int right = grid[i][j] + dfs(i+1,j+1,grid,dp);
+
+        return dp[i][j] = min(down,right);
+    }
+    int minimumTotal(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid.size();
+        vector<vector<int>> dp(m,vector<int>(n,0));
         //tabulation
-        vector<vector<int>> dp(triangle.size(), vector<int>(triangle.size(),-1));
-        dp[0][0] = triangle[0][0];
-        for(int i = 0 ; i < triangle.size(); i++){
-            for(int j = 0 ; j < triangle[i].size(); j++){
-                if(i == 0 && j == 0) continue;
-                
-                if(j >= 1 && j < triangle[i].size()-1){
-                    dp[i][j] = min(triangle[i][j] + dp[i-1][j] , triangle[i][j] + dp[i-1][j-1]);
-                } else if(j == triangle[i].size() - 1){
-                    dp[i][j] = triangle[i][j]+dp[i-1][j-1];
-                } else {
-                    dp[i][j] = triangle[i][j]+dp[i-1][j];
+        dp[0][0] = grid[0][0];
+        for(int i = 1; i < m; i++){
+            for(int j = 0; j <= i; j++){
+                int up = 1e9;
+                if(j < i){
+                    up = grid[i][j] + dp[i-1][j];
                 }
+                int right = 1e9;
+                if(j >= 1){
+                    right = grid[i][j] + dp[i-1][j-1];
+                }
+
+                dp[i][j] = min(up,right);
             }
         }
-        int minsum = INT_MAX;
-        for(int i = 0 ; i < triangle.size(); i++){
-            minsum = min(dp[triangle.size()-1][i] , minsum);
+        int ans = 1e9;
+
+        for(int i : dp[m-1]){
+            ans = min(ans,i);
         }
-        return minsum;
+
+        return ans;
     }
 };
