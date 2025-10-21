927026db29acbddda9f2dc901d159bb7fb0ff8b0
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-10-21 20:16:55 +0530

    Time: 0 ms (100%), Space: 13.5 MB (6.39%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,41 +1,31 @@
 class Solution {
 public:
-    int dfs(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &dp){
-        if(i >= grid.size()){
-            return 0;
-        }
-
-        if(j >= grid[i].size()) return 0;
-
-        if(dp[i][j] != -1) return dp[i][j];
-        int down = grid[i][j] + dfs(i+1,j,grid,dp);
-        int right = grid[i][j] + dfs(i+1,j+1,grid,dp);
-
-        return dp[i][j] = min(down,right);
-    }
     int minimumTotal(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid.size();
-        vector<vector<int>> dp(m,vector<int>(n,0));
+        vector<int> dp(1,0);
         //tabulation
-        dp[0][0] = grid[0][0];
+        dp[0] = grid[0][0];
         for(int i = 1; i < m; i++){
+            vector<int> cur(i+1);
+            dp.resize(i+1);
             for(int j = 0; j <= i; j++){
                 int up = 1e9;
                 if(j < i){
-                    up = grid[i][j] + dp[i-1][j];
+                    up = grid[i][j] + dp[j];
                 }
                 int right = 1e9;
                 if(j >= 1){
-                    right = grid[i][j] + dp[i-1][j-1];
+                    right = grid[i][j] + dp[j-1];
                 }
 
-                dp[i][j] = min(up,right);
+                cur[j] = min(up,right);
             }
+            dp = cur;
         }
         int ans = 1e9;
 
-        for(int i : dp[m-1]){
+        for(int i : dp){
             ans = min(ans,i);
         }
 
