e9b30439cfb42f58e9455ef2096ddd3e706ab945
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-11-16 16:53:56 +0530

    Time: 43 ms (41.61%), Space: 73.2 MB (5.01%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    long long dfs(int i, vector<int> &coins, int amt, vector<vector<int>> &dp){
-        if(i == 0){
-            if(amt%coins[i] == 0){
-                return amt/coins[i];
-            } 
-            return INT_MAX;
-        } 
+    int coinChange(vector<int>& coins, int amount){
+        vector<long long> dp(amount + 1);
+        for(int i = 0; i <= amount; i++){
+            if(i%coins[0] == 0){
+                dp[i] = i/coins[0];
+            } else {
+                dp[i] = INT_MAX;
+            }
+        }
+        for(int i = 1; i < coins.size(); i++){
+            vector<long long> roll(amount + 1);
+            for(int j = 0; j <= amount; j++){
+                long long take = INT_MAX;
+                if(j >= coins[i]){
+                    take = 1 + roll[j-coins[i]];
+                }
+                long long not_take = dp[j];
 
-        if(amt == 0) return 0;
-        if(amt < 0) return INT_MAX;
-        if(dp[i][amt] != -1) return dp[i][amt]; 
-        long long take = INT_MAX;
-        long long not_take = 0;
-        if(amt >= coins[i]){
-            take = 1 + dfs(i,coins,amt-coins[i], dp);
-        } 
+                roll[j] = min(take,not_take);
+            }
+            dp = roll;
+        }
 
-        not_take = dfs(i-1,coins,amt, dp);
-        return dp[i][amt] = min(take,not_take);
-    }
-    int coinChange(vector<int>& coins, int amount) {
-        sort(coins.begin(), coins.end());
-        vector<vector<int>> dp(coins.size(), vector<int>(amount + 1, -1));
-        int ans = dfs(coins.size() - 1, coins, amount, dp);
-        if(ans >= INT_MAX) return -1;
-        return ans;
+        if(dp[amount] >= INT_MAX) return -1;
+        return dp[amount];
     }
 };
