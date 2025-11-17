55a246c01d37daf1b1a390937d79c96f6bbfeb30
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-11-17 07:05:47 +0530

    Time: 3 ms (10.4%), Space: 10.6 MB (11.06%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
-    bool checkValidString(string s) {
-        int min = 0, max = 0;
-        for (char c : s){
-            if(c == '('){
-                min++;
-                max++;
-            } else if(c == ')'){
-                min--;
-                max--;
-            } else {
-                min--;
-                max++;
-            }
+    bool dfs(int i, string &s, int count, vector<vector<int>> &dp){
+        if(count < 0) return false;
 
-
-            if(min < 0) min = 0;
-            if (max < 0) return false;
+        if(i == s.size()){
+            return count == 0;
         }
 
-        return min == 0;
+        if(dp[i][count] != -1) return dp[i][count];
+
+        if(s[i] == '(') {
+            return dfs(i+1, s, count + 1, dp);
+        } 
+        if(s[i] == ')') {
+            return dfs(i+1,s,count-1, dp);
+        }
+
+        bool empty = dfs(i+1,s,count, dp);
+        bool open = dfs(i+1, s, count+1, dp);
+        bool close = dfs(i+1,s,count-1, dp);
+
+        return dp[i][count] = empty || open || close;
+    }
+    bool checkValidString(string s) {
+        vector<vector<int>> dp(s.size(), vector<int>(s.size() + 1,-1));
+        return dfs(0,s,0,dp);
     }
 };
