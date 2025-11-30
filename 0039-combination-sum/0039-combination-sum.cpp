20257d98f945fe6f2341b7a8fb0456eec443ec72
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-11-30 11:29:09 +0530

    Time: 101 ms (5.29%), Space: 69.6 MB (8.07%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    void recursion(int sum , int i, int target, vector<int> curr, vector<int> &nums, vector<vector<int>> &ans){
-            if(sum == target) {
-                ans.push_back(curr);
-                return ;
-            }
-        if(i >= nums.size() || sum > target) return ;
-        recursion(sum,i+1,target,curr,nums,ans);
-        curr.push_back(nums[i]);
-        sum += nums[i];
-        recursion(sum,i,target,curr,nums,ans);
+    void dfs(int i, vector<int> candidates, vector<vector<int>> &ans, vector<int> res, int target){
+        if(target == 0){
+            ans.push_back(res);
+            return ;
+        }
+        if(target < 0 || i >= candidates.size()) return;
+
+        //take and not_take
+        if(target >= candidates[i]){
+            res.push_back(candidates[i]);
+            dfs(i,candidates, ans, res, target-candidates[i]);
+            res.pop_back();
+        }
+        dfs(i+1,candidates,ans,res,target);
+
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        //same as power set
         vector<vector<int>> ans;
-        recursion(0,0,target,{},candidates,ans); 
-        return ans;  
+        dfs(0,candidates,ans,{},target);
+        return ans;
     }
 };
