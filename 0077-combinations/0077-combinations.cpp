87a9e81f042122b705e599cff1b78bfa090129c6
saketh-18 <142143584+saketh-18@users.noreply.github.com>
2025-11-30 11:28:28 +0530

    Time: 65 ms (45.31%), Space: 109.9 MB (24.81%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void dfs(int n, int k, vector<vector<int>> &ans, vector<int> &res){
+        if(res.size() == k){
+            ans.push_back(res);
+            return ;
+        }
+
+        if(n <= 0) return;
+
+        //take the current n and reduce the n and not take and reduce it
+        if(res.size() < k){
+            res.push_back(n);
+            dfs(n-1,k,ans,res);
+            res.pop_back();
+        }
+
+        dfs(n-1,k,ans,res);
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> ans;
+        vector<int> res;
+        dfs(n,k,ans,res);
+        return ans;
+    }
+};
